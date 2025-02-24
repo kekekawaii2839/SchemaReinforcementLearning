f58d4360ae703491242e0ab1397eef5121a1bce1
LHLKEVIN0713 <lihaolun000000@126.com>
2025-02-25 00:35:13 +0800

    add srl training script

--- scripts/train_sft.sh
+++ scripts/train_sft.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 python train/sft/trl_mix.py \
     --model_name meta-llama/Llama-3.2-3B-Instruct \
     --data_file ../data/mix_train.json \
-    --output_dir results/llama3.2-3b-sft \
+    --output_dir ../results/llama3.2-3b-sft \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
